34cd47a6efdd56477647e49f53ea67078f4a9e64
Apprentice <apprentice@dbc08.local>
2012-11-13 22:41:40 -0800

    Added validation to Student class for email format and uniqueness and age

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,17 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+ 
+  validates :email, :format => {:with => /^.+@.+\...+$/ }
+  validates :email, :uniqueness => true
+  validates :age, :numericality => { :greater_than => 4 }
+
+
   def name
      "#{first_name} #{last_name}"
   end
 
   def age
-     x = ((Time.now.to_date - birthday)/365).to_i
+     ((Time.now.to_date - birthday)/365).to_i
   end
 end
