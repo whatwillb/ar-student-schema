d4d1f4ecbc09f42feac195533c36b83acc1f9c06
Apprentice <apprentice@dbc08.local>
2012-11-13 19:08:36 -0800

    Created a new migration for the student table.
    This is my first time using Migration.create_table!

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,16 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+    create_table(:students) do |t|
+      t.column :id, :integer
+      t.column :first_name, :string
+      t.column :last_name, :string
+      t.column :gender, :string
+      t.column :email, :string
+      t.column :phone, :string
+      t.column :birthday, :date
+    end
+
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
